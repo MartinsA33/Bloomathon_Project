532670c2b11acec4187a2e09e8ac9bfc1a66084e
Martins Akinbowale <akinbowalemartins1@gmail.com>
2025-07-22 18:43:56 -0400

    Eight commit: contains the finished sliding feature

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelector(".dot1").style.backgroundColor="black";
 });
 
-let clicked=0;
+
 document.querySelector(".Left-button").addEventListener("click", function(){
-    clicked++;
-    console.log("clicked");
+
     if(document.querySelector(".dot1").style.backgroundColor==="black") {
         document.querySelector(".dot1").style.backgroundColor = "transparent";
         document.querySelector(".dot5").style.backgroundColor = "black";
@@ -22,14 +21,56 @@ document.querySelector(".Left-button").addEventListener("click", function(){
     else if(document.querySelector(".dot4").style.backgroundColor==="black") {
         document.querySelector(".dot4").style.backgroundColor = "transparent";
         document.querySelector(".dot3").style.backgroundColor = "black";
+        document.querySelector(".Self-Esteem-div").style.display = "none";
+        document.querySelector(".health-div").style.display = "block";
     }
     else if(document.querySelector(".dot3").style.backgroundColor==="black") {
         document.querySelector(".dot3").style.backgroundColor = "transparent";
         document.querySelector(".dot2").style.backgroundColor = "black";
+        document.querySelector(".health-div").style.display = "none";
+        document.querySelector(".relationship-div").style.display = "block";
     }
     else if(document.querySelector(".dot2").style.backgroundColor==="black") {
         document.querySelector(".dot2").style.backgroundColor = "transparent";
         document.querySelector(".dot1").style.backgroundColor = "black";
+        document.querySelector(".relationship-div").style.display = "none";
+        document.querySelector(".mental-div").style.display = "block";
     }
+
+});
+
+document.querySelector(".Right-button").addEventListener("click", function(){
+
+    if(document.querySelector(".dot1").style.backgroundColor==="black") {
+        document.querySelector(".dot1").style.backgroundColor = "transparent";
+        document.querySelector(".dot2").style.backgroundColor = "black";
+        document.querySelector(".mental-div").style.display = "none";
+        document.querySelector(".relationship-div").style.display = "block";
+    }
+    else if(document.querySelector(".dot2").style.backgroundColor==="black") {
+        document.querySelector(".dot2").style.backgroundColor = "transparent";
+        document.querySelector(".dot3").style.backgroundColor = "black"
+        document.querySelector(".relationship-div").style.display = "none";
+        document.querySelector(".health-div").style.display = "block";
+    }
+    else if(document.querySelector(".dot3").style.backgroundColor==="black") {
+        document.querySelector(".dot3").style.backgroundColor = "transparent";
+        document.querySelector(".dot4").style.backgroundColor = "black";
+        document.querySelector(".health-div").style.display = "none";
+        document.querySelector(".Self-Esteem-div").style.display = "block";
+    }
+    else if(document.querySelector(".dot4").style.backgroundColor==="black") {
+        document.querySelector(".dot4").style.backgroundColor = "transparent";
+        document.querySelector(".dot5").style.backgroundColor = "black";
+        document.querySelector(".Self-Esteem-div").style.display = "none";
+        document.querySelector(".Purpose-div").style.display = "block";
+    }
+    else if(document.querySelector(".dot5").style.backgroundColor==="black") {
+        document.querySelector(".dot5").style.backgroundColor = "transparent";
+        document.querySelector(".dot1").style.backgroundColor = "black";
+        document.querySelector(".Purpose-div").style.display = "none";
+        document.querySelector(".mental-div").style.display = "block";
+    }
+
 
 });
